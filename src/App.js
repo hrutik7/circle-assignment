2d1e62bfe6f26f5ab2974a99df75923b3f6fde13
hrutik7 <hrutik.kumthekar19@vit.edu>
2022-04-21 20:49:02 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className='border__box'>
+
+      
+      <div className='squre_1'>
+
+        <div className='circle'>
+
+        </div>
+
+      </div>
+
+
+      <div className='squre_2'>
+
+      </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
